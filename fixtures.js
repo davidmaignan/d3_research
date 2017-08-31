08e545fc7450dee25c1f1c546c0ee844261686d0
David Maignan <David.Maignan@transat.com>
2017-08-31 09:36:31 -0400

    initial commit

---
+++ fixtures.js
@@ -0,0 +1,685 @@
+var fixtures = {
+  "sensorsTypes": [
+    { "1": "http" },
+    { "2": "httpWebFullPage" },
+    { "3": "json" }
+  ],
+  "components": [
+    {
+      "id": 0,
+      "name": "TSPrimaryData feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 1,
+      "name": "FSPrimaryData feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 2,
+      "name": "Flight routes feed",
+      "sensorsIds": [ ],
+      "dependencies": [ 17 ]
+    },
+    {
+      "id": 3,
+      "name": "Flight calendar feed",
+      "sensorsIds": [ ],
+      "dependencies": [ 16, 18 ]
+    },
+    {
+      "id": 4,
+      "name": "Campaign Builder feed",
+      "sensorsIds": [ ],
+      "dependencies": [ 17, 18, 19, 20 ]
+    },
+    {
+      "id": 5,
+      "name": "Best prices feed",
+      "sensorsIds": [ ],
+      "dependencies": [ 19, 20 ]
+    },
+    {
+      "id": 6,
+      "name": "Seat selection feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 7,
+      "name": "Geolocation feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 8,
+      "name": "Hotel factsheet detail feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 9,
+      "name": "Hotel factsheet list feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 10,
+      "name": "Hotel factsheet summary feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 11,
+      "name": "Itinerary factsheet detail feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 12,
+      "name": "Itinerary factsheet list feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 13,
+      "name": "Itinerary factsheet product feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 14,
+      "name": "Eco fare marketing content feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 15,
+      "name": "DTX PA Marketing content feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 16,
+      "name": "HDV file",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 17,
+      "name": "Flight route Radixx table import",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 18,
+      "name": "Flight calendar Radixx table import",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 19,
+      "name": "PPPpJ Radixx table import",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 20,
+      "name": "PPPpM Radixx table import",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 21,
+      "name": "TDP flight booking process",
+      "sensorsIds": [ ],
+      "dependencies": [ 67 ]
+    },
+    {
+      "id": 22,
+      "name": "TDP seat selection to come",
+      "sensorsIds": [ ],
+      "dependencies": [ 15 ]
+    },
+    {
+      "id": 23,
+      "name": "BE seat selection standalone",
+      "sensorsIds": [ ],
+      "dependencies": [ 6 ]
+    },
+    {
+      "id": 24,
+      "name": "SV flight booking process",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 25,
+      "name": "SV package booking process",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 26,
+      "name": "TS home page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 51, 58, 64, 65 ]
+    },
+    {
+      "id": 27,
+      "name": "TS deals pages",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 51, 58, 62, 64, 65 ]
+    },
+    {
+      "id": 28,
+      "name": "TS cheap flights pages",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 44, 51, 58, 62, 64, 65 ]
+    },
+    {
+      "id": 29,
+      "name": "TS destination pages",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 68 ]
+    },
+    {
+      "id": 30,
+      "name": "TS travel info pages",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 31,
+      "name": "TS seat selection page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 23 ]
+    },
+    {
+      "id": 32,
+      "name": "TS family and friends pages",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 60, 61 ]
+    },
+    {
+      "id": 33,
+      "name": "TS web checkin page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 66 ]
+    },
+    {
+      "id": 34,
+      "name": "TS flight status page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 47 ]
+    },
+    {
+      "id": 35,
+      "name": "TS flight calendar page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 46 ]
+    },
+    {
+      "id": 36,
+      "name": "TS group quote pages",
+      "sensorsIds": [ ],
+      "dependencies": [ 52, 53, 54, 55 ]
+    },
+    {
+      "id": 37,
+      "name": "TS my booking page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 61 ]
+    },
+    {
+      "id": 38,
+      "name": "TS kids club pages",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 56, 57 ]
+    },
+    {
+      "id": 39,
+      "name": "TS secure flight page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 94 ]
+    },
+    {
+      "id": 40,
+      "name": "TS newsletter page",
+      "sensorsIds": [ ],
+      "dependencies": [ 42, 69 ]
+    },
+    {
+      "id": 41,
+      "name": "TS DO pages",
+      "sensorsIds": [ ],
+      "dependencies": [ 8, 9, 10, 11, 12, 13 ]
+    },
+    {
+      "id": 42,
+      "name": "Component Header/Footer",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 43,
+      "name": "Component Alerts",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 44,
+      "name": "Widget Best price histogram TS",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 5 ]
+    },
+    {
+      "id": 45,
+      "name": "Widget Best price histogram TO",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 46,
+      "name": "Widget CDV",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 3, 7 ]
+    },
+    {
+      "id": 47,
+      "name": "Widget SDV",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 7 ]
+    },
+    {
+      "id": 48,
+      "name": "Widget Destination stats",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 49,
+      "name": "Widget Eco Fares",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 50,
+      "name": "Widget Edoc",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 51,
+      "name": "Widget Flight deals",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 4, 7, 67 ]
+    },
+    {
+      "id": 52,
+      "name": "Widget Group Quote - Information",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 53,
+      "name": "Widget Group Quote - Option",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 7 ]
+    },
+    {
+      "id": 54,
+      "name": "Widget Group Quote - Review",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 55,
+      "name": "Widget Group Quote - Progress tracker",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 56,
+      "name": "Widget Kids club - Subscription",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 57,
+      "name": "Widget Kids club - Unsubscription",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 58,
+      "name": "Widget Links rows",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 59,
+      "name": "Widget Family and Friends - Login",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 60,
+      "name": "Widget Family and Friends - Send invitation",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 61,
+      "name": "Widget My booking",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 62,
+      "name": "Widget Related destinations",
+      "sensorsIds": [ ],
+      "dependencies": [ 0 ]
+    },
+    {
+      "id": 63,
+      "name": "Widget Rich content",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 94,
+      "name": "Widget Secure flight",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 64,
+      "name": "Widget Showbox",
+      "sensorsIds": [ ],
+      "dependencies": [ 4, 5, 7, 67 ]
+    },
+    {
+      "id": 65,
+      "name": "Widget Stratos booking engine",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 7 ]
+    },
+    {
+      "id": 66,
+      "name": "Widget Web checkin",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 67,
+      "name": "Component Traveler selector",
+      "sensorsIds": [ ],
+      "dependencies": [ 84 ]
+    },
+    {
+      "id": 68,
+      "name": "Component Media zone",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 69,
+      "name": "Widget Newsletter",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 70,
+      "name": "Widget External Newsletter",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 71,
+      "name": "Widget External Edoc",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 72,
+      "name": "Widget External Stratos booking engine for Canoe",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 2, 7 ]
+    },
+    {
+      "id": 73,
+      "name": "Widget External Stratos booking engine for TDP",
+      "sensorsIds": [ ],
+      "dependencies": [ 0, 2, 7 ]
+    },
+    {
+      "id": 74,
+      "name": "Widget External Stratos booking engine for Destination",
+      "sensorsIds": [ ],
+      "dependencies": [ 2, 7 ]
+    },
+    {
+      "id": 93,
+      "name": "Widget External Stratos booking engine for TO Portal",
+      "sensorsIds": [ ],
+      "dependencies": [ 2, 7 ]
+    },
+    {
+      "id": 75,
+      "name": "Mobile App destination timeline feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 76,
+      "name": "Mobile App hotel timeline feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 77,
+      "name": "Mobile App destinations menu feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 78,
+      "name": "Mobile App page contact us feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 79,
+      "name": "Mobile App airport list feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 80,
+      "name": "TO Portal extraction feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 81,
+      "name": "TO Portal flight engine feeds",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 82,
+      "name": "TDP dynamic contents",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 83,
+      "name": "SV dynamic contents",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 84,
+      "name": "Flight search feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 85,
+      "name": "Alerts feed",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 86,
+      "name": "Site externe Canoe",
+      "sensorsIds": [ ],
+      "dependencies": [ 72, 90 ]
+    },
+    {
+      "id": 87,
+      "name": "Site externe Marlin travel",
+      "sensorsIds": [ ],
+      "dependencies": [ 90, 70 ]
+    },
+    {
+      "id": 88,
+      "name": "Site externe Kayak",
+      "sensorsIds": [ ],
+      "dependencies": [ 84 ]
+    },
+    {
+      "id": 89,
+      "name": "Site externe Cartrawler",
+      "sensorsIds": [ ],
+      "dependencies": [ 90 ]
+    },
+    {
+      "id": 90,
+      "name": "Header/Footer dynamic contents",
+      "sensorsIds": [ ],
+      "dependencies": [ ]
+    },
+    {
+      "id": 91,
+      "name": "Mobile App",
+      "sensorsIds": [ ],
+      "dependencies": [ 75, 76, 77, 78, 79 ]
+    },
+    {
+      "id": 92,
+      "name": "TO Portal",
+      "sensorIds": [ ],
+      "dependencies": [ 80, 81, 84, 93 ]
+    }
+  ],
+
+  "groups": [
+    {
+      "groupId": 1,
+      "name": "TS.WCM",
+      "componentIds": [ ],
+      "groupIds": [ 11, 12, 13, 14, 15 ]
+    },
+    {
+      "groupId": 2,
+      "name": "TS.API",
+      "componentIds": [ ],
+      "groupIds": [ 9, 10 ]
+    },
+    {
+      "groupId": 3,
+      "name": "Datalex",
+      "componentIds": [ 21, 22 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 4,
+      "name": "Soft Voyage",
+      "componentIds": [ 24, 25 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 6,
+      "name": "Backend",
+      "componentIds": [ 16, 23 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 7,
+      "name": "Radixx DB",
+      "componentIds": [ 17, 18, 19, 20 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 9,
+      "name": "External Web sites",
+      "componentIds": [ 86, 87, 88, 89 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 10,
+      "name": "TS API Flight",
+      "componentIds": [ 0, 1, 2, 3, 4, 5, 6, 7, 14, 15 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 11,
+      "name": "TS API TO",
+      "componentIds": [ 8, 9, 10, 11, 12, 13 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 12,
+      "name": "TS components",
+      "componentIds": [ 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 13,
+      "name": "TS external widgets",
+      "componentIds": [ 70, 71, 72, 73, 74 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 14,
+      "name": "TS services pages",
+      "componentIds": [ 30, 33, 34, 35, 37, 39 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 15,
+      "name": "TS sale pages",
+      "componentIds": [ 26, 27, 28, 29, 31, 32, 36, 38, 40, 41 ],
+      "groupIds": [ ]
+    },
+    {
+      "groupId": 17,
+      "name": "TS WCM feeds",
+      "componentIds": [ 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 90 ],
+      "groupIds": [ ]
+    }
+  ],
+
+  "sensors": [
+    {
+      "id": "4316",
+      "name": "getAirports",
+      "sensorType": "1",
+      "url": "http://stg-api.airtransat.com/en-CA/Services/TSPrimaryData/GetAirports",
+      "timeOut": "20"
+    },
+    {
+      "id": "4133",
+      "name": "airTransat",
+      "sensorType": "httpWebFullPage",
+      "url": "https://www.airtransat.com/fr-CA/reserver/reserver-un-vol",
+      "timeOut": "60"
+    }
+  ]
+}
+
+export { fixtures };
---
+++ index.js
@@ -0,0 +1,8 @@
+
+import * as d3 from 'd3';
+import {fixtures} from './fixtures.js'
+
+const square = d3.selectAll("rect")
+square.style("fill", "red")
+
+// var datas = fixtures
