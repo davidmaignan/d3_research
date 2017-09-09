db7dbd5ee12d5fe6a845f7466e420a6b51e1bd7d
David Maignan <davidmaignan@gmail.com>
2017-09-09 16:13:27 -0400

    add pop over and mouseover event

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ var radius = 6;             // fixed node radius
 var yfixed = pad + radius;  // y position for all nodes
 
 let configuration = {
-  "width": 2000,
-  "height": 800,
+  "width": 2500,
+  "height": 2000,
   "node":{
     "x": 0,
     "y": 160
@@ -59,12 +59,16 @@ function flattenGroupComponentList(list, g){
   })
 
   g.components.forEach((cmp) => {
+    links.add({'source': g, 'target': cmp})
     flattenComponentListRec(list, cmp)
   })
 }
-function flattenComponentListRec(list, cmp){
-  list.add(cmp)
-  cmp.dependencies.forEach((cmp) => flattenComponentListRec(list, cmp))
+function flattenComponentListRec(list, component){
+  list.add(component)
+  component.dependencies.forEach((cmp) => {
+    links.add({'source': component, 'target': cmp})
+    flattenComponentListRec(list, cmp)
+  })
 }
 
 for(let i = 0; i < groupTopLevel.length - 1; i++){
@@ -98,6 +102,13 @@ var nodeContainer = svg.append("g")
          "transform": `translate(10, 10)`,
        })
 
+var popover = d3.select("body")
+       	.append("div")
+        .attr('class', 'popover')
+       	.style("position", "absolute")
+       	.style("z-index", "10")
+       	.style("visibility", "hidden")
+
 nodeContainer.selectAll(".node")
        .data(nodes)
        .enter()
@@ -107,6 +118,18 @@ nodeContainer.selectAll(".node")
        .attr("cx", function(d, i) { return i * 20 })
        .attr("cy", function(d, i) { return configuration.node.y; })
        .attr("r",  function(d, i) { return radius; })
+       .on("mouseover", function(c){
+         popover.style("visibility", "visible")
+         popover.style("left", c.x)
+         popover.style("top", "200px")
+         popover.html(`<ul><li>id: ${c.id}</li><li>${c.name}</li></ul>`)
+
+         let circle = d3.select(`circle[id='${c.name}']`)
+         console.log(circle)
+         circle.style('fill', 'red')
+
+       }).
+       on('mouseout', c => {popover.style("visibility", "hidden")})
        .style("fill",   function(d, i) { return color(d.group); })
 
 nodeContainer.selectAll(".text")
@@ -136,45 +159,37 @@ var arc = d3.lineRadial()
 
 
 nodeContainer.selectAll(".link")
-        .data(links.toArray())
-        .enter()
-        .append("path")
-        .styles({
-          "fill": "none",
-          "stroke": "#888888",
-          "stroke-weight": "1px",
-          "stroke-opacity": 0.5
-        })
-        .attr("class", "link")
-        .attr("transform", function(d, i) {
+  .data(links.toArray())
+  .enter()
+  .append("path")
+  .styles({
+    "fill": "none",
+    "stroke": "#888888",
+    "stroke-weight": "1px",
+    "stroke-opacity": 0.5
+  })
+  .attr("class", "link")
+  .attr("transform", function(d, i) {
+      d.source.x = parseInt(d3.select(`circle[id='${d.source.name}']`).attr("cx"))
+      d.target.x = parseInt(d3.select(`circle[id='${d.target.name}']`).attr("cx"))
 
-            console.log(d.source.name)
+      var xshift = d.source.x + (d.target.x - d.source.x) / 2;
+      var yshift = 160;
+      return "translate(" + xshift + ", " + yshift + ")";
+  })
+  .attr("d", function(d, i) {
+      // get x distance between source and target
+      var xdist = Math.abs(d.source.x - d.target.x);
 
-            let sourceNode = d3.select(`circle[id='${d.source.name}']`);
+      // set arc radius based on x distance
+      arc.radius(xdist / 2);
 
+      // want to generate 1/3 as many points per pixel in x direction
+      var points = d3.range(0, Math.ceil(xdist / 3));
 
-            console.log(d3.select(`circle[id='${d.target.name}']`).attr("cx"))
+      // set radian scale domain
+      radians.domain([0, points.length - 1]);
 
-            d.source.x = parseInt(d3.select(`circle[id='${d.source.name}']`).attr("cx"))
-            d.target.x = parseInt(d3.select(`circle[id='${d.target.name}']`).attr("cx"))
-
-            var xshift = d.source.x + (d.target.x - d.source.x) / 2;
-            var yshift = 160;
-            return "translate(" + xshift + ", " + yshift + ")";
-        })
-        .attr("d", function(d, i) {
-            // get x distance between source and target
-            var xdist = Math.abs(d.source.x - d.target.x);
-
-            // set arc radius based on x distance
-            arc.radius(xdist / 2);
-
-            // want to generate 1/3 as many points per pixel in x direction
-            var points = d3.range(0, Math.ceil(xdist / 3));
-
-            // set radian scale domain
-            radians.domain([0, points.length - 1]);
-
-            // return path for arc
-            return arc(points);
-        });
+      // return path for arc
+      return arc(points);
+  });
